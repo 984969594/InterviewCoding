7acb6e427b28034ca978ceb0d26ec7d9a1c2a60c
chenjianfeng <984969594@qq.com>
2020-07-01 16:43:31 +0800

    项目新建

---
+++ pro/leetcode-pro/src/main/java/com/sgps/leetcode/leetcodepro/LeetcodeProApplication.java
@@ -0,0 +1,13 @@
+package com.sgps.leetcode.leetcodepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeProApplication.class, args);
+	}
+
+}
